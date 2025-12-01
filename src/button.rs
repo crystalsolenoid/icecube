66b1542ec503e36c2ce30b66cde8886b5234dbb7
John Christensen <johnedchristensen@gmail.com>
2025-11-30 19:07:09 -0600

    Add whenever_down button event

--- src/button.rs
+++ src/button.rs
@@ -5,6 +5,7 @@ pub struct Button<Message> {
     /// Pressed on the most recent frame
     on_press: Option<Message>,
     on_hover: Option<Message>,
+    whenever_down: Option<Message>,
 }
 
 impl<Message> Button<Message> {
@@ -12,6 +13,7 @@ impl<Message> Button<Message> {
         Self {
             on_press: None,
             on_hover: None,
+            whenever_down: None,
         }
     }
 
@@ -26,19 +28,26 @@ impl<Message> Button<Message> {
         self.on_hover = Some(m);
         self
     }
+
+    pub fn whenever_down(mut self, m: Message) -> Self {
+        self.whenever_down = Some(m);
+        self
+    }
 }
 
 impl<Message: Clone> Element<Message> for Button<Message> {
     fn draw(&self, _frame: &mut [u8], _region: CalculatedLayout) {}
     fn get_message(&mut self, input: &Input, region: CalculatedLayout) -> Option<Message> {
         if let Some(mouse_pos) = input.mouse_pos {
-            if input.mouse_released && region.contains(mouse_pos) {
-                self.on_press.clone()
-            } else {
-                None
+            if region.contains(mouse_pos) {
+                if input.mouse_released {
+                    return self.on_press.clone();
+                };
+                if input.mouse_down {
+                    return self.whenever_down.clone();
+                }
             }
-        } else {
-            None
         }
+        return None;
     }
 }
